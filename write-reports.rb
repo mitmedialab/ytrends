2f7ea299b101a8134a5a98baf91f33d30199275c
Rahul Bhargava <rahulb@media.mit.edu>
2013-05-17 23:02:11 -0400

    comment

--- write-reports.rb
+++ write-reports.rb
@@ -3,4 +3,5 @@ require 'ytrends'
 
 reporter = Ytrends::Reporter.new
 
+# passes in the weight threshold (ie. no edges with a common_pct less than this threshold are exported)
 reporter.render_all({:weight => 0.3})
