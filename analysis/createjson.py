0daeb88f2bbc3f22ba6d069ccc10d36ee95e19d1
Edward L Platt <ed@elplatt.com>
2013-08-29 14:20:19 -0400

    Adds mock data to www/data.

---
+++ analysis/createjson.py
@@ -0,0 +1,45 @@
+from operator import itemgetter
+import json
+import sqlalchemy
+import networkx as nx
+import ytrends.locations as locs
+import ytrends.graph as graph
+from ytrends.db import *
+
+# init the connection to the database
+engine = sqlalchemy.create_engine("sqlite:///db/development.sqlite3", echo=True)
+Session = sqlalchemy.orm.sessionmaker(bind=engine)
+session = Session()
+
+# Query ranks for all videos
+ranks = session.query(Rank.video_id, Rank.loc, sqlalchemy.sql.func.count('*').label('entries')).\
+    filter(sqlalchemy.not_(Rank.loc.like('%all_%'))).\
+    filter_by(source='view').\
+    group_by(Rank.video_id, Rank.loc)
+
+count_by_loc = {}
+for rank in ranks:
+    videos = count_by_loc.get(rank[1], {})
+    videos[rank[0]] = rank[2]
+    count_by_loc[rank[1]] = videos
+
+results = {}
+for source in count_by_loc.keys():
+    for target in count_by_loc.keys():
+        if source == target:
+            continue
+        # Get distance
+        videos = set(count_by_loc[source].keys()).\
+            intersection(set(count_by_loc[target].keys()))
+        weights = [(v, min(count_by_loc[source][v], count_by_loc[target][v])) for v in videos]
+        source_res = results.get(source, {})
+        weight = sum([w[1] for w in weights])
+        if weight > 0:
+            source_res[target] = {
+                'w': weight,
+                'v': [w[0] for w in sorted(weights, key=lambda x: x[1], reverse=True)][0:10]
+            }
+            results[source] = source_res
+
+with open('output/weights-mincut.json', 'wb') as f:
+    f.write(json.dumps(results))
