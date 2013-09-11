b083ee11ebf8fa9f1ec94546cf2582dc52ab6ff0
Edward L Platt <ed@elplatt.com>
2013-09-11 16:35:13 -0400

    Move database queries into stats.py

---
+++ analysis/stats.py
@@ -0,0 +1,35 @@
+import sqlalchemy
+
+from ytrends.db import *
+
+# init the connection to the database
+engine = sqlalchemy.create_engine("sqlite:///db/development.sqlite3", echo=True)
+Session = sqlalchemy.orm.sessionmaker(bind=engine)
+session = Session()
+
+def get_day_count_by_country():
+    # Query ranks for all days per country
+    country_days = session.query(Rank.loc, sqlalchemy.sql.func.count(sqlalchemy.sql.func.distinct(Rank.date))).\
+        filter(sqlalchemy.not_(Rank.loc.like('%all_%'))).\
+        filter_by(source='view').\
+        group_by(Rank.loc)
+    day_count_by_country = {}
+    for country_day in country_days:
+        s = country_day[0]
+        day_count_by_country[s] = country_day[1]
+    return day_count_by_country
+
+def get_count_by_loc():
+    # Query ranks for all videos
+    ranks = session.query(Rank.video_id, Rank.loc, sqlalchemy.sql.func.count('*').label('entries')).\
+        filter(sqlalchemy.not_(Rank.loc.like('%all_%'))).\
+        filter_by(source='view').\
+        group_by(Rank.video_id, Rank.loc)
+    
+    # Create dict with counts (location -> video id -> count)
+    count_by_loc = {}
+    for rank in ranks:
+        videos = count_by_loc.get(rank[1], {})
+        videos[rank[0]] = rank[2]
+        count_by_loc[rank[1]] = videos
+    return count_by_loc
