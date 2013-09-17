329c8d110d7bbc07a6acffc5829431e4dd105007
Rahul Bhargava <rahulb@media.mit.edu>
2013-09-17 10:57:23 -0400

    indentation fix

--- www/app/routes.js
+++ www/app/routes.js
@@ -1,36 +1,36 @@
 App.CountryRouter = Backbone.Router.extend({
 
-  routes: {
-    "":                    "home",
-    ":country1":            "exploreCountry",
-    ":country1/:country2":  "exploreRelated"
-  },
+    routes: {
+        "":                       "home",
+        ":country1":              "exploreCountry",
+        ":country1/:country2":    "exploreRelated"
+    },
 
-  home: function(){
-    App.debug("Routed to root");
-    App.mapView.handleMapBackgroundClick();
-  },
+    home: function(){
+        App.debug("Routed to root");
+        App.mapView.handleMapBackgroundClick();
+    },
 
-  exploreCountry: function(country1Alpha3) {
-    App.debug("Routed to "+country1Alpha3);
-    var country1 = App.allCountries.findByAlpha3(country1Alpha3);
-    if(country1){
-      App.mapView.handleValidCountryClick(country1);
-    } else {
-      //TODO
+    exploreCountry: function(country1Alpha3) {
+        App.debug("Routed to "+country1Alpha3);
+        var country1 = App.allCountries.findByAlpha3(country1Alpha3);
+        if(country1){
+            App.mapView.handleValidCountryClick(country1);
+        } else {
+            //TODO
+        }
+    },
+
+    exploreRelated: function(country1Alpha3, country2Alpha3) {
+        App.debug("Routed to "+country1Alpha3+"/"+country2Alpha3);
+        var country1 = App.allCountries.findByAlpha3(country1Alpha3);
+        var country2 = App.allCountries.findByAlpha3(country2Alpha3);
+        if(country1 && country2){
+            App.mapView.handleValidCountryClick(country1);
+            App.mapView.handleValidCountryClick(country2);
+        } else {
+            //TODO
+        }
     }
-  },
-
-  exploreRelated: function(country1Alpha3, country2Alpha3) {
-    App.debug("Routed to "+country1Alpha3+"/"+country2Alpha3);
-    var country1 = App.allCountries.findByAlpha3(country1Alpha3);
-    var country2 = App.allCountries.findByAlpha3(country2Alpha3);
-    if(country1 && country2){
-      App.mapView.handleValidCountryClick(country1);
-      App.mapView.handleValidCountryClick(country2);
-    } else {
-      //TODO
-    }
-  }
 
 });
